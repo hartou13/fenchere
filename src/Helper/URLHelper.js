93cbfc4586ef17b179592d9425a4dae1e58053c0
hartou13 <hartrand13@gmail.com>
2023-02-03 23:35:07 +0300

    Update URLHelper.js

--- src/Helper/URLHelper.js
+++ src/Helper/URLHelper.js
@@ -2,7 +2,7 @@ class URLHelper  {
     static urlgen(url){
       //  console.log("http://localhost/phpRH/"+url);
         // return "https://enchere20-production.up.railway.app/"+url;
-        return "http://localhost:3000/"+url;
+        return "https://enchere20-production.up.railway.app/"+url;
     }    
 }
  
